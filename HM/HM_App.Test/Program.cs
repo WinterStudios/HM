cef5f505f9c8e7e403064101b30816b0d7612474
Hugo Marques <hugo_filipe_marques@live.com.pt>
2020-10-12 22:25:25 +0100

    fix test

--- HM/HM_App.Test/Program.cs
+++ HM/HM_App.Test/Program.cs
@@ -7,15 +7,18 @@ namespace HM_App.Test
     {
         static void Main(string[] args)
         {
+            
             switch (args[0])
             {
                 case "-output":
                     switch (args[1])
                     {
                         case "-versionToGitHub":
-                            string assemblyLocation = "../HM_App/bin/Debug/"
+                            string assemblyLocation = AppDomain.CurrentDomain.BaseDirectory + "HM_App.dll";
+                            Console.WriteLine(assemblyLocation);
                             string version = System.Diagnostics.FileVersionInfo.GetVersionInfo(assemblyLocation).FileVersion;
-                            Console.WriteLine(string.Format("v{0}",API.SemVersion.))
+                            Console.WriteLine(string.Format("v{0}", API.SemVersion.GetVersionFromAssembly(version)));
+                            break;
                     }
                     break;
 
